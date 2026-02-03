edee34a2bebf73a9675596354678286bb9d0bb7d
gabrielngch <gabriel.ng.chong.hui@gmail.com>
2026-02-03 17:41:17 +0000

    chore: default to bind-mounted examples via compose

--- examples/tasks/sample_tasks.py
+++ examples/tasks/sample_tasks.py
@@ -6,8 +6,3 @@ from scheduler.decorators import schedule
 @schedule(timedelta(seconds=10))
 def hello_task() -> None:
     print("hello from scheduler")
-
-
-@schedule(timedelta(seconds=15))
-def second_task() -> None:
-    print("second task ran")
