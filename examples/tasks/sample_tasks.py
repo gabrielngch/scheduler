63e2240e50640713c2203f84278aedf405c09743
gabrielngch <gabriel.ng.chong.hui@gmail.com>
2026-02-03 17:27:56 +0000

    chore: add second example task

--- examples/tasks/sample_tasks.py
+++ examples/tasks/sample_tasks.py
@@ -6,3 +6,8 @@ from scheduler.decorators import schedule
 @schedule(timedelta(seconds=10))
 def hello_task() -> None:
     print("hello from scheduler")
+
+
+@schedule(timedelta(seconds=15))
+def second_task() -> None:
+    print("second task ran")
